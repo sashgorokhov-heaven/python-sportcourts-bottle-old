88791885a732b1ff2079528740819853cf3b0eaf
Vitaliy Kharchenko <harchenko.grape@gmail.com>
2014-08-20 14:12:48 +0600

    Срастил валидаторы в один скрипт

--- static/view/js/bootstrap_validator.js
+++ static/view/js/bootstrap_validator.js
@@ -45,3 +45,27 @@ $(document).ready(function () {
         });
     $('#profileForm').bootstrapValidator();
 });
+
+$(document).ready(function() {
+    $('#gameaddForm')
+        .bootstrapValidator({
+            feedbackIcons: {
+                valid: 'glyphicon glyphicon-ok',
+                invalid: 'glyphicon glyphicon-remove',
+                validating: 'glyphicon glyphicon-refresh'
+            },
+            fields: {
+                gender: {
+                    validators: {
+                        notEmpty: {
+                            message: 'Пол необходимо указать'
+                        }
+                    }
+                },
+            }
+        });
+});
+
+$(document).ready(function() {
+    $('#courtaddForm').bootstrapValidator();
+});
