a24df096ee0080f3e298d06223be04ccb6362c57
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-08-23 13:01:39 +0600

    Special admin page for showing pure templates

---
+++ pages/showtemplate.py
@@ -0,0 +1,20 @@
+import bottle
+
+import pages
+
+
+class ShowTemplate(pages.Page):
+    path = ['showtpl']
+
+    def execute(self, method:str):
+        if method == 'GET':
+            data = self.get()
+            if isinstance(data, pages.Template):
+                return data.template()
+            return data
+
+    @pages.setlogin
+    def get(self):
+        if 'name' not in bottle.request.query or pages.getadminlevel() == 1:
+            raise bottle.HTTPError(404)
+        return pages.Template(bottle.request.query.get("name"))
