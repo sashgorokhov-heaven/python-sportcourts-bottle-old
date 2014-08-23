e2fd46bc6905d348878c06ca7556f2f85306c16d
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-08-23 13:04:55 +0600

    Fix

--- pages/showtemplate.py
+++ pages/showtemplate.py
@@ -15,6 +15,6 @@ class ShowTemplate(pages.Page):
 
     @pages.setlogin
     def get(self):
-        if 'name' not in bottle.request.query or pages.getadminlevel() == 1:
+        if 'name' not in bottle.request.query or pages.getadminlevel() != 1:
             raise bottle.HTTPError(404)
         return pages.Template(bottle.request.query.get("name"))
