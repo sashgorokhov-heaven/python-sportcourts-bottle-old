855589b56f6d752717e857294e4e02c5e1e7139d
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-09-06 20:28:03 +0600

    Function for paging

--- modules/__init__.py
+++ modules/__init__.py
@@ -11,6 +11,17 @@ import bottle
 config = json.load(open(os.path.join('modules', 'config.json'), 'r'))
 
 
+def pager(page, count:int=8) -> tuple:
+    """
+    page:int starts from 1 - 1,2,3...
+    count - items per page
+
+    :return offset, count (ready to use in LIMIT)
+    """
+    page -= 1
+    return page * count, count
+
+
 def _generate_secret():
     return ''.join(
         random.sample(list(itertools.chain(map(chr, range(65, 91)), map(chr, range(97, 122)), map(str, range(1, 10)))),
