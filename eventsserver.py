fbd866208eb592be28d2c7e6a95ee14aeca7bdf2
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-09-10 00:16:09 +0600

    stopping event server by get request

--- eventsserver.py
+++ eventsserver.py
@@ -2,6 +2,12 @@ import bottle, sys, eventslib
 
 config = sys.argv[1]
 
+
+@bottle.get('/stop')
+def stop():
+    eventslib.event_server.stop()
+
+
 eventslib.event_server.start()
 
 application = bottle.default_app()
