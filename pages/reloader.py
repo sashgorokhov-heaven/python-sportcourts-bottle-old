f715d4935dfd94acf6e364cc72668747f3ac1727
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-09-17 22:49:44 +0600

    Fixed permissions in reloader

--- pages/reloader.py
+++ pages/reloader.py
@@ -5,7 +5,7 @@ import pages
 
 class Reloader(pages.Page):
     def get(self, page_name:str):
-        if pages.auth_dispatcher.organizer():
+        if pages.auth_dispatcher.admin():
             pages.controller.reload(page_name)
             raise bottle.redirect('/')
         raise bottle.HTTPError(404)
