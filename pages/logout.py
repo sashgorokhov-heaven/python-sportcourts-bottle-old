31ab34a92274b87c8e89a9f763b58fe828f5f244
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-09-17 23:45:15 +0600

    New logout redirect

--- pages/logout.py
+++ pages/logout.py
@@ -6,6 +6,6 @@ import pages
 class Logout(pages.Page):
     def get(self):
         pages.auth_dispatcher.logout()
-        raise bottle.redirect('/')
+        raise bottle.redirect(bottle.request.get_header("Referer", "/"))
 
     get.route = '/logout'
