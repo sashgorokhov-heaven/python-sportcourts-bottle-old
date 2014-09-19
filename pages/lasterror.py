1db331d80e17d7f6a77ee1467ecbe491ca7a7b7e
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-09-20 00:26:18 +0600

    Page for viewing errors added

--- pages/lasterror.py
+++ pages/lasterror.py
@@ -1,14 +1,14 @@
 import bottle
 import os
 import pages
-from modules import logging
+import modules.logging
 
 
 class LastError(pages.Page):
     def get(self, **kwargs):
-        if not pages.auth_dispatcher.admin() or not os.path.exists(logging._TRACEBACK_FILE):
+        if not pages.auth_dispatcher.admin() or not os.path.exists(modules.logging._TRACEBACK_FILE):
             raise bottle.HTTPError(404)
-        error = open(logging._TRACEBACK_FILE, 'r').read()
+        error = open(modules.logging._TRACEBACK_FILE, 'r').read()
         return pages.templates.message("Последняя ошибка: ", error.replace("\n", "<br>"))
 
     get.route = '/lasterror'
