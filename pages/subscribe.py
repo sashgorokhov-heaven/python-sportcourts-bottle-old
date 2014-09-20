65b9b3f1abcbbca79798295c2751828400d3610f
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-09-20 20:43:43 +0600

    informing responsibles

--- pages/subscribe.py
+++ pages/subscribe.py
@@ -2,8 +2,9 @@ import bottle
 from modules.utils import beautifuldate, beautifultime, beautifulday
 
 import pages
-from models import games, notifications, sport_types
+from models import games, notifications, users
 from modules import dbutils, create_link
+import datetime, itertools
 
 
 class Subscribe(pages.Page):
@@ -44,6 +45,18 @@ class Subscribe(pages.Page):
             game['parsed_datetime'] = (beautifuldate(game['datetime'], True),
                                        beautifultime(game['datetime']),
                                        beautifulday(game['datetime']))
+            pdatetime = datetime.datetime(*itertools.chain(map(int, game['datetime'].split(' ')[0].split('-')),
+                                                           map(int, game['datetime'].split(' ')[1].split(':'))))
+            if pdatetime - datetime.timedelta(
+                    days=1) <= datetime.datetime.now() <= pdatetime and user_id != pages.auth_dispatcher.getuserid():
+                user = users.get(user_id, fields=['user_id', 'first_name', 'last_name'], dbconnection=db)
+                if not unsubscribe:
+                    message = 'На завтрашнюю игру "{}" записался {}'.format(create_link.game(game),
+                                                                            create_link.user(user))
+                else:
+                    message = '{} отписался от завтрашней игры "{}"'.format(create_link.user(user),
+                                                                            create_link.game(game))
+                notifications.add(game['responsible_user']['user_id'], message, 1, dbconnection=db)
             return pages.PageBuilder("game", tab_name=tab_name, game=game)
 
 
