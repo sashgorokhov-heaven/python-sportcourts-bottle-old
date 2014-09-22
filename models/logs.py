fc1a28a83522b663d3ec88475ed7009486b28994
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-09-22 18:43:03 +0600

    delete

--- models/logs.py
+++
@@ -1,7 +0,0 @@
-from models import autodb
-from modules import dbutils
-
-
-@autodb
-def add(user_id:int, text:str, dbconnection:dbutils.DBConnection=None):
-    dbconnection.execute("INSERT INTO logs (user_id, text) VALUES ({}, '{}')".format(user_id, text))
