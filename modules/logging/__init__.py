709c4b1b4b00cc104cf55906560a2163c8690bc9
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-09-28 01:34:03 +0600

    uwsgi spooler support

--- modules/logging/__init__.py
+++ modules/logging/__init__.py
@@ -1,37 +1,43 @@
 import socket, bottle, pages, modules
 from modules.logging import speaker
-
+import uwsgi
 
 def _send_(data:dict):
+    data = _decode_data(data)
+    data['user_id'] = int(data['user_id'])
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(0.2)
     addr = (modules.config['log_server']['ip'], int(modules.config['log_server']['port']))
     try:
         sock.connect(addr)
         session = speaker.Session(sock, addr)
-        try:
-            session.send(modules.config['log_server']['passwd'])
-            if session.recieve()['status'] == 1:
-                return session.close()
-            session.send({'data': data})
-        except:
-            pass
-        finally:
-            session.close()
+        session.send(modules.config['log_server']['passwd'])
+        if session.recieve()['status'] == 1:
+            return session.close()
+        session.send({'data': data})
     except:
-        pass
+        return uwsgi.SPOOL_RETRY
     finally:
         try:
             sock.close()
         except:
             pass
+    return uwsgi.SPOOL_OK
+
+
+def _encode_data(data:dict) -> dict:
+    data = dict(zip(map(lambda x: x.encode(), data), map(lambda x: str(data[x]).encode(), data)))
+    return data
+
+
+def _decode_data(data:dict) -> dict:
+    data.pop('spooler_task_name')
+    data = dict(zip(map(lambda x: x.decode(), data), map(lambda x: data[x].decode(), data)))
+    return data
 
 
 def _send(data:dict):
-    try:
-        _send_(data)
-    except:
-        pass
+    uwsgi.spool(_encode_data(data), spooler='/bsp/spool_log')
 
 
 def _access_data() -> dict:
@@ -63,3 +69,6 @@ def access():
 def error(error:Exception):
     data = _error_data(error)
     _send(data)
+
+
+uwsgi.spooler = _send_
