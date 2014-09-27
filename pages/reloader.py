eb0ac58f554fb822281f19fec0010de455327938
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-09-28 01:45:57 +0600

    new reloading features

--- pages/reloader.py
+++ pages/reloader.py
@@ -1,11 +1,12 @@
-import bottle
-
-import pages
+import bottle, pages, uwsgi
 
 
 class Reloader(pages.Page):
     def get(self, page_name:str):
         if pages.auth_dispatcher.admin():
+            if page_name == 'all':
+                uwsgi.reload()
+                raise bottle.redirect(bottle.request.get_header("Referer", "/"))
             pages.controller.reload(page_name)
             raise bottle.redirect(bottle.request.get_header("Referer", "/"))
         raise bottle.HTTPError(404)
