05694d5ebd9e37cc38b48e55be8a1654cf82934b
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-09-28 14:41:10 +0600

    log reader app

---
+++ logreader.py
@@ -0,0 +1,42 @@
+from modules import dbutils
+import modules, time, base64
+
+
+def print_row(row):
+    line = '[{}] {} "{} {}"'.format(*row[1:5])
+    if row[6]:
+        line += ' uid={}'.format(row[6])
+    print(line)
+    if row[7]:
+        print('#' * 50)
+        print()
+        try:
+            print('\t\t', row[7])
+        except:
+            print(ascii(row[7]))
+        try:
+            print('\t\t', row[2])
+        except:
+            print(ascii(row[2]))
+        print()
+        try:
+            print(base64.b64decode(row[9].encode()).decode())
+        except:
+            print_row(ascii(base64.b64decode(row[9].encode()).decode()))
+        print('#' * 50, flush=True)
+        print()
+
+
+with dbutils.dbopen(host=modules.config['logdb']['dbhost'],
+                    user=modules.config['logdb']['dbuser'],
+                    passwd=modules.config['logdb']['dbpasswd'],
+                    db=modules.config['logdb']['dbname'],
+                    charset='utf8') as db:
+    last_id = db.execute("SELECT MAX(id) FROM access;")[0][0]
+    while True:
+        rows = db.execute("SELECT * FROM access WHERE id>{}".format(last_id))
+        if len(rows) > 0:
+            for row in rows:
+                print_row(row)
+            last_id = rows[-1][0]
+        time.sleep(1)
