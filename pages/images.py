f22fb4676ecc3ee3ed01be4dce0a0d5640055e64
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-10-05 04:34:03 +0600

    Trying to add caching in avatars, it doesnt work yet

--- pages/images.py
+++ pages/images.py
@@ -1,16 +1,47 @@
 import os
 import bottle
+import hashlib
 
 import pages
 from models import games
 
 
-def no_cache(func):
+_hashes = dict() # filename -> hash
+
+
+def _hash(filename:str) -> bytes:
+    hasher = hashlib.md5()
+    with open(filename, 'rb') as f:
+        while True:
+            b = f.read(1024)
+            if not b:
+                break
+            hasher.update(b)
+    return hasher.hexdigest()
+
+def _can_cache(filename:str):
+    current_hash = _hash(filename)
+    if filename in _hashes:
+        if current_hash == _hashes[filename]:
+            return True
+        else:
+            _hashes[filename] = current_hash
+            return False
+    else:
+        _hashes[filename] = current_hash
+        return True
+
+def cache(func):
     def wrapper(*args, **kwargs):
-        resp = func(*args, **kwargs)
+        resp, filename = func(*args, **kwargs)
+        #print('Cache', filename, '? ', end='')
+        #if not _can_cache(filename):
+            #print('Nope')
         resp.set_header('Cache-Control', 'no-cache, no-store, must-revalidate')
         resp.set_header('Pragma', 'no-cache')
         resp.set_header('Expires', 0)
+        #else:
+        #    print('Yep')
         return resp
     return wrapper
 
@@ -22,17 +53,17 @@ class Images(pages.Page):
     def get_static_image(self, name):
         return bottle.static_file(name, '/bsp/data/images/static/')
 
-    @no_cache
+    @cache
     def get_avatar_image(self, name):
         filename = str(name)
         dirname = '/bsp/data/images/avatars'
         fullaname = os.path.join(dirname, filename+'.jpg')
         if not os.path.exists(fullaname):
             filename = 'blank'
-            return bottle.static_file('{}.jpg'.format(filename), '/bsp/data/images/avatars/')
+            return bottle.static_file('{}.jpg'.format(filename), dirname), os.path.join(dirname, '{}.jpg'.format(filename))
         if 'sq' in bottle.request.query:
             filename = filename+'_sq'
-        return bottle.static_file('{}.jpg'.format(filename), '/bsp/data/images/avatars/')
+        return bottle.static_file('{}.jpg'.format(filename), dirname), os.path.join(dirname, '{}.jpg'.format(filename))
 
     def get_og_image(self, name):
         return bottle.static_file(name, '/bsp/data/images/og/')
