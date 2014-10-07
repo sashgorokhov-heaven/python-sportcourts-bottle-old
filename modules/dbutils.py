043a2f7d51b1a7f83fd7602af1abfafe5ece3f5c
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-10-08 05:26:55 +0600

    removed unuceful code

--- modules/dbutils.py
+++ modules/dbutils.py
@@ -67,35 +67,4 @@ def _setdbfields():
         return {table: [i[0] for i in db.execute('SHOW FIELDS FROM {};'.format(table))] for table in tables}
 
 
-def strdates(dictobject:dict):
-    for i in dictobject:
-        if isinstance(dictobject[i], datetime.date) or \
-                isinstance(dictobject[i], datetime.time) or \
-                isinstance(dictobject[i], datetime.datetime):
-            dictobject[i] = str(dictobject[i])
-
-
-class get:
-    def __init__(self, db:dbopen):
-        self._db = db
-
-    def city(self, city_id:int) -> list:
-        return self._db.execute("SELECT * FROM cities WHERE city_id={}".format(city_id), dbfields['cities'])
-
-    def user(self, user_id:int) -> list:
-        return self._db.execute("SELECT * FROM users WHERE user_id={}".format(user_id), dbfields['users'])
-
-    def court(self, court_id:int) -> list:
-        return self._db.execute("SELECT * FROM courts WHERE court_id={}".format(court_id), dbfields['courts'])
-
-    def game_type(self, type_id:int) -> list:
-        return self._db.execute("SELECT * FROM game_types WHERE type_id={}".format(type_id), dbfields['game_types'])
-
-    def game(self, game_id:int) -> list:
-        return self._db.execute("SELECT * FROM games WHERE game_id={}".format(game_id), dbfields['games'])
-
-    def sport_type(self, sport_id:int) -> list:
-        return self._db.execute("SELECT * FROM sport_types WHERE sport_id={}".format(sport_id), dbfields['sport_types'])
-
-
 dbfields = _setdbfields()
