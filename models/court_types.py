7c53c2fddb1aae45e28e25a4931c2296d72f4192
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-10-10 23:05:11 +0600

    court_types added

---
+++ models/court_types.py
@@ -0,0 +1,37 @@
+import dbutils
+from models import autodb, splitstrlist, Cache
+from objects import CourtType
+
+
+_cache = Cache(600, 'type_id')
+
+
+@_cache
+@autodb
+def get(type_id, dbconnection:dbutils.DBConnection=None) -> list:
+    if isinstance(type_id, str) and len(type_id.split(',')) > 0:
+        type_id = splitstrlist(type_id)
+        if len(type_id) == 1:
+            type_id = type_id[0]
+
+    if isinstance(type_id, list) and len(type_id)==0: return list()
+
+    if isinstance(type_id, int) and type_id != 0:
+        dbconnection.execute("SELECT * FROM court_types WHERE type_id='{}'".format(type_id),
+                             dbutils.dbfields['sport_types'])
+    elif isinstance(type_id, list):
+        dbconnection.execute(
+            "SELECT * FROM court_types WHERE type_id IN (" + ','.join(map(str, type_id)) + ")",
+            dbutils.dbfields['sport_types'])
+    elif type_id == 0:
+        dbconnection.execute("SELECT * FROM court_types", dbutils.dbfields['sport_types'])
+
+    if len(dbconnection.last()) == 0: return list()
+
+    court_types = dbconnection.last()
+    court_types = list(map(lambda x: CourtType(x), court_types))
+
+    if isinstance(type_id, int) and type_id != 0:
+        return court_types[0]
+    elif isinstance(type_id, list) or type_id == 0:
+        return court_types
