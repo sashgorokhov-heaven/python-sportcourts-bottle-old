675e80755c23a7ab4145342ac7cee60922a922c9
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-10-17 21:21:57 +0600

    fix

--- dbutils.py
+++ dbutils.py
@@ -94,10 +94,10 @@ class dbopen:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self._db.close()
 
-def _setdbfields(kwargs=default_connection):
+def setdbfields(kwargs=default_connection):
     with dbopen(**kwargs) as db:
         tables = [i[0] for i in db.execute('SHOW TABLES;')]
         return {table: [i[0] for i in db.execute('SHOW FIELDS FROM {};'.format(table))] for table in tables}
 
 
-dbfields = _setdbfields()
+dbfields = setdbfields()
