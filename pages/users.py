967ea3841a94d2e14d32098bae0861c01e788782
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-10-22 22:40:50 +0600

    removed unuseful code

--- pages/users.py
+++ pages/users.py
@@ -11,29 +11,13 @@ USERS_PER_PAGE = 8
 
 class Users(pages.Page):
     def get(self):
-        with dbutils.dbopen() as db:
-            paging = {'previous': 0, 'next': 0}
-            count = int(db.execute("SELECT COUNT(user_id) FROM users")[0][0])
-            total_pages = count // USERS_PER_PAGE + 1
-            page_n = 1
-            if 'page' in bottle.request.query:
-                page_n = int(bottle.request.query.get('page'))
-                if not 0 < page_n <= total_pages:
-                    raise bottle.HTTPError(404)
-                if page_n != total_pages:
-                    paging['next'] = page_n + 1
-                if page_n > 1:
-                    paging['previous'] = page_n - 1
-            else:
-                if total_pages > 1:
-                    paging['next'] = 2
-            allusers = users.get(0, count=slice(*modules.pager(page_n, count=USERS_PER_PAGE)))
-            page = pages.PageBuilder('users', allusers=allusers, paging=paging, count=count)
-            if pages.auth.loggedin():
-                if len(pages.auth.current().friends())>0:
-                    friends = pages.auth.current().friends(True)
-                    page.add_param('myfriends', friends)
-            return page
+        allusers = users.get(0, count=slice(*modules.pager(1, count=USERS_PER_PAGE)))
+        page = pages.PageBuilder('users', allusers=allusers)
+        if pages.auth.loggedin():
+            if len(pages.auth.current().friends())>0:
+                friends = pages.auth.current().friends(True)
+                page.add_param('myfriends', friends)
+        return page
 
     def post(self):
         if bottle.request.is_ajax:
