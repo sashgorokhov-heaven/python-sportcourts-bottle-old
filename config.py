1b45e3695abc7e8c99ed5f7357aed88f8196f723
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-10-31 13:48:35 +0600

    path check in config

--- config.py
+++ config.py
@@ -13,6 +13,21 @@ if not os.path.exists(os.path.join(configpath, 'serverconfig.py')):
 from serverconfig import *
 
 
+_notfound = list()
+for class_name in filter(lambda x: not x.startswith('_'), paths.__dict__):
+    class_type = getattr(paths, class_name)
+    for pathattr in filter(lambda x: not x.startswith('_'), class_type.__dict__):
+        path = getattr(class_type, pathattr)
+        if not os.path.exists(path):
+            _notfound.append(('paths.{}.{}'.format(class_name, pathattr), path))
+if len(_notfound)>0:
+    for path in _notfound:
+        try:
+            print('{} = "{}" Not found!'.format(*path))
+        except:
+            raise FileNotFoundError(path[1])
+
+
 def _generate_secret():
     return ''.join(
         random.sample(list(itertools.chain(map(chr, range(65, 91)), map(chr, range(97, 122)), map(str, range(1, 10)))),
@@ -21,3 +36,4 @@ def _generate_secret():
 
 if not debug:
     secret = _generate_secret()
+
