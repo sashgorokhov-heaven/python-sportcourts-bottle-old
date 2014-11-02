4604f8c9f41d7d7ec801e41bad8a807d4c38f7f9
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-11-02 23:38:45 +0400

    users

--- pages/admin.py
+++ pages/admin.py
@@ -1,8 +1,14 @@
+import bottle
 import pages
-
+import dbutils
 
 class Admin(pages.Page):
     def get(self, **kwargs):
-        return pages.PageBuilder('admin')
+        if not pages.auth.current().userlevel.admin():
+            raise bottle.HTTPError(404)
+        with dbutils.dbopen() as db:
+            users = db.execute("SELECT user_id, first_name, last_name, email, phone  FROM users")
+            users = list(map(lambda x: [x[0], x[1]+' '+x[2], x[3], x[4]], users))
+            return pages.PageBuilder('admin', users=users)
 
     get.route = '/admin'
