56e7e05c19ea64ccd25b9b4a12c77cbffac4ea7a
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-11-13 17:58:44 +0500

    fixes

--- pages/blog.py
+++ pages/blog.py
@@ -7,14 +7,16 @@ import base64
 
 class Blog(pages.Page):
     def get(self, **kwargs):
+        if not pages.auth.current().userlevel.admin():
+            raise bottle.HTTPError(404)
         return pages.PageBuilder('blog')
 
     def post(self):
+        if not pages.auth.current().userlevel.admin():
+            raise bottle.HTTPError(404)
         with dbutils.dbopen() as db:
-            text = base64.b64encode(bottle.request.forms.get('text'))
-            sql = 'INSERT INTO blog_posts (content) VALUES ({contentval})'
-            sql = sql.format(
-                contentval=text)
+            text = base64.b64encode(bottle.request.forms.get('text')).decode()
+            sql = 'INSERT INTO blog_posts (content) VALUES ({})'.format(text)
             db.execute(sql)
             raise bottle.redirect("/blog")
 
