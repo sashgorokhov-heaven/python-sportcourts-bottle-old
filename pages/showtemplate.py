77878f7d06752944ae230b9b9245d0ceabe09e76
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-11-15 02:41:28 +0500

    new request routing way

--- pages/showtemplate.py
+++ pages/showtemplate.py
@@ -1,12 +1,6 @@
-import bottle
-
 import pages
 
 
-class ShowTemplate(pages.Page):
-    def get(self, name:str):
-        if not pages.auth.current().userlevel.admin():
-            raise bottle.HTTPError(404)
-        return pages.PageBuilder(name)
-
-    get.route = '/showtpl/<name>'
+@pages.only_admins
+def show_template(name:str):
+    return pages.PageBuilder(name)
