83eda7e0ab23940d3dc53bd9c2e73ca8e725a31b
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-11-15 20:18:21 +0500

    new request routing way

--- pages/list.py
+++ pages/list.py
@@ -1,18 +1,15 @@
 import bottle
-
 import dbutils
 import pages
 from models import games
 
 
-class List(pages.Page):
-    def get(self, game_id:int):
-        with dbutils.dbopen() as db:
-            game = games.get_by_id(game_id, dbconnection=db)
-            if len(game) == 0:
-                raise bottle.HTTPError(404)
-            if pages.auth.current().user_id() != game.created_by() and pages.auth.current().user_id() != game.responsible_user_id() and not pages.auth.current().userlevel.admin():
-                return pages.templates.permission_denied()
-            return pages.PageBuilder('list', game=game)
-
-    get.route = '/list/<game_id:int>'
+@pages.get('/list/<game_id:int>')
+def subscribed_list(game_id:int):
+    with dbutils.dbopen() as db:
+        game = games.get_by_id(game_id, dbconnection=db)
+        if len(game) == 0:
+            raise bottle.HTTPError(404)
+        if pages.auth.current().user_id() != game.created_by() and pages.auth.current().user_id() != game.responsible_user_id() and not pages.auth.current().userlevel.admin():
+            return pages.templates.permission_denied()
+        return pages.PageBuilder('list', game=game)
