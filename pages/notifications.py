05d9537a2951f6567ed012d848eccfa3a1d03f08
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-11-15 20:46:47 +0500

    new request routing way

--- pages/notifications.py
+++ pages/notifications.py
@@ -1,39 +1,33 @@
 import bottle
-
 import pages
 import dbutils
 import models.notifications
 
 
-class Notifications(pages.Page):
-    def execute(self, method:str, **kwargs):
-        if not pages.auth.loggedin():
-            return pages.templates.permission_denied().template()
-        return super().execute(method, **kwargs)
-
-    def get(self):
-        user_id = pages.auth.current().user_id()
-        with dbutils.dbopen() as db:
-            if 'deleteall' in bottle.request.query:
-                models.notifications.delete(-user_id)
-                raise bottle.redirect('/notifications')
-            count = models.notifications.get_count(user_id, dbconnection=db)
-            notifications = dict()
-            notifications['all'] = models.notifications.get(user_id, type=0, all=count == 0, dbconnection=db)
-            notifications['subscribed'] = models.notifications.get(user_id, type=1, all=count == 0, dbconnection=db)
-            if pages.auth.current().userlevel.resporgadmin():
-                notifications['responsible'] = models.notifications.get(user_id, type=2, all=count == 0,
-                                                                        dbconnection=db)
-            return pages.PageBuilder("notifications", notifications=notifications, all=count == 0)
-
-    def post(self):
-        if 'read' in bottle.request.forms:
-            notification_id = int(bottle.request.forms.get('read'))
-            models.notifications.read(notification_id)
-        if 'delete' in bottle.request.forms:
-            notification_id = int(bottle.request.forms.get('delete'))
-            models.notifications.delete(notification_id)
+@pages.get('/notifications')
+@pages.only_loggedin
+def get():
+    user_id = pages.auth.current().user_id()
+    with dbutils.dbopen() as db:
+        if 'deleteall' in bottle.request.query:
+            models.notifications.delete(-user_id)
+            raise bottle.redirect('/notifications')
+        count = models.notifications.get_count(user_id, dbconnection=db)
+        notifications = dict()
+        notifications['all'] = models.notifications.get(user_id, type=0, all=count == 0, dbconnection=db)
+        notifications['subscribed'] = models.notifications.get(user_id, type=1, all=count == 0, dbconnection=db)
+        if pages.auth.current().userlevel.resporgadmin():
+            notifications['responsible'] = models.notifications.get(user_id, type=2, all=count == 0,
+                                                                    dbconnection=db)
+        return pages.PageBuilder("notifications", notifications=notifications, all=count == 0)
 
 
-    get.route = '/notifications'
-    post.route = get.route
+@pages.post('/notifications')
+@pages.only_loggedin
+def post():
+    if 'read' in bottle.request.forms:
+        notification_id = int(bottle.request.forms.get('read'))
+        models.notifications.read(notification_id)
+    if 'delete' in bottle.request.forms:
+        notification_id = int(bottle.request.forms.get('delete'))
+        models.notifications.delete(notification_id)
