a0e191c5a5f006c062389b9e5e60fce341f76087
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-11-15 20:26:51 +0500

    new request routing way

--- pages/blog.py
+++ pages/blog.py
@@ -1,24 +1,25 @@
 import bottle
-
 import dbutils
 import pages
-
 import base64
 
-class Blog(pages.Page):
-    def get(self, **kwargs):
-        if not pages.auth.current().userlevel.admin():
-            raise bottle.HTTPError(404)
-        return pages.PageBuilder('blog')
 
-    def post(self):
-        if not pages.auth.current().userlevel.admin():
-            raise bottle.HTTPError(404)
-        with dbutils.dbopen() as db:
-            text = base64.b64encode(bottle.request.forms.get('text').encode()).decode()
-            sql = 'INSERT INTO blog_posts (content) VALUES ({})'.format(text)
-            db.execute(sql)
-            raise bottle.redirect("/blog")
+@pages.get(['/blog', '/blog/'])
+def get_blog(): # для всего блога
+    return pages.PageBuilder('blog')
 
-    get.route = '/blog'
-    post.route = '/blog'
+
+@pages.post('/blog')
+@pages.only_admins
+def post_blog(): # добавление новости
+    with dbutils.dbopen() as db:
+        text = base64.b64encode(bottle.request.forms.get('text').encode()).decode()
+        sql = 'INSERT INTO blog_posts (content) VALUES ({})'.format(text)
+        db.execute(sql)
+        article_id = db.execute('SELECT last_insert_id() FROM games')[0][0]
+        raise bottle.redirect("/blog/{}".format(article_id))
+
+
+@pages.get('/blog/<article_id:int>')
+def get_article(article_id:int): # просмотр отдельной новости
+    raise NotImplementedError
