2e388ec6e9616603c468eec39fccef6dd4436c3f
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-11-15 20:36:12 +0500

    new request routing way

--- pages/groups.py
+++ pages/groups.py
@@ -1,8 +1,17 @@
 import pages
 
 
-class Groups(pages.Page):
-    def get(self, **kwargs):
-        return pages.PageBuilder('groups')
+@pages.get('/groups')
+def groups():
+    return pages.PageBuilder('groups')
 
-    get.route = '/groups'
+
+@pages.post('/groups')
+@pages.only_organizers
+def add_group():
+    raise NotImplementedError
+
+
+@pages.get('/groups/<group_id:int>')
+def group(group_id:int):
+    raise NotImplementedError
