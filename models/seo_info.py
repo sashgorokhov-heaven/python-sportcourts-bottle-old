33f13858b9b9d95685232d3c7b07e5875168e6c3
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-12-06 06:31:02 +0500

    fix

--- models/seo_info.py
+++ models/seo_info.py
@@ -7,6 +7,6 @@ import cacher
 @cacher.create('seo_info', 600, cacher.KeyCache)
 def get(tplname:str, *args) -> SeoInfo:
     with dbutils.dbopen() as db:
-        db.execute("SELECT * FROM seo_info WHERE tplname='{}'".format(tplname))
+        db.execute("SELECT * FROM seo_info WHERE tplname='{}'".format(tplname), dbutils.dbfields['seo_info'])
         if len(db.last()) == 0: return None
         return SeoInfo(db.last()[0])
