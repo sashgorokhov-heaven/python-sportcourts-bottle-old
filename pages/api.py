29df98c33f4e1dafd75608f9a77c31839618251d
Alexander Gorokhov <sashgorokhov@gmail.com>
2015-01-04 14:31:19 +0500

    some work

--- pages/api.py
+++ pages/api.py
@@ -7,6 +7,7 @@ import pages
 import dbutils
 import json
 from modules import logging
+from models import users, cities, ampluas, decode_set
 
 
 def dump(obj) -> str:
@@ -44,11 +45,19 @@ class Error(Exception):
     def invalid_ip(ip:str):
         return Error(4, 'Invalid ip {}'.format(ip))
 
+    @staticmethod
+    def restricted_fields(fields:list):
+        return Error(5, 'Restricted fields:{}'.format(','.join(fields)))
+
+#    @staticmethod
+#    def invalid_field(fields:list):
+#        return Error(6, 'Invalid fields:{}'.format(','.join(fields)))
+
     def json(self) -> dict:
         ret = {'code':self.code, 'description':self.description}
         if self.e:
             ret['eclass'] = self.e.__class__.__name__
-            ret['eargs'] = '<{}>'.format(','.join(self.e.args)) if len(self.e.args)>0 else ''
+            ret['eargs'] = '{}'.format(','.join(list(map(str,self.e.args)))) if len(self.e.args)>0 else ''
         return ret
 
 
@@ -83,7 +92,7 @@ def check_auth(func):
             if len(au)>0:
                 if bottle.request.remote_addr!=au[0]['ip']:
                     raise Error.invalid_ip(bottle.request.remote_addr)
-                if datetime.datetime.now()-au[0]['datetime']>datetime.timedelta(days=1):
+                if datetime.datetime.now()-au[0]['datetime']>datetime.timedelta(days=7):
                     raise Error.invalid_token()
                 return func(*args, **kwargs)
             raise Error.invalid_token()
@@ -97,16 +106,83 @@ def strdatetime(d:dict) -> dict:
     return d
 
 
+def current_user(db:dbutils.DBConnection, detalized:bool=False) -> users.User:
+    token = bottle.request.query.get('token')
+    user_id = db.execute("SELECT user_id FROM api_auth WHERE token='{}'".format(token))[0][0]
+    if detalized:
+        return users.get(user_id, dbconnection=db)
+    return user_id
+
+
+_available_fields = {
+    "users_get": {
+        "restricted": {'email', 'passwd', 'settings', 'referer'}
+    },
+    "courts_get": {
+        "restricted": {'cost', 'phone', 'admin_description'}
+    },
+    "games_get": {
+        "restricted": {'notificated'}
+    }
+}
+
+
+def check_fields_access(fields:list, key:str, db:dbutils.DBConnection):
+    fields = set(fields)
+    f = _available_fields[key]
+    user = current_user(db, True)
+    if 'available' in f:
+        f = f['available']
+        if not fields.issubset(f) and not user.userlevel.admin():
+            r = set(f).difference_update(f)
+            raise Error.restricted_fields(r)
+    elif 'restricted' in f:
+        f = f['restricted']
+        if not fields.isdisjoint(f) and not user.userlevel.admin():
+            r = set(fields).intersection(f)
+            raise Error.restricted_fields(r)
+
+
+class getters:
+    @staticmethod
+    def city(city_id:int, db:dbutils.DBConnection) -> dict:
+        city = cities.get(city_id, dbconnection=db)
+        return {"city_id": city.city_id(), "title": city.title(), "geopoint": city.geopoint()}
+
+    @staticmethod
+    def court(court_id:int, db:dbutils.DBConnection) -> dict:
+        pass
+
+
 @pages.get('/api/users/get')
 @handle_error
 @check_auth
 def users_get():
-    fields = bottle.request.query.get('fields') if 'fields' in bottle.request.query else 'user_id'
+    fields = bottle.request.query.get('fields') if 'fields' in bottle.request.query else '*'
     fields = fields.split(',')
+
     with dbutils.dbopen() as db:
-        users = db.execute("SELECT {} FROM users".format(', '.join(fields)), fields)
-        list(map(strdatetime, users))
-        return dump({'count':len(users), 'users':users})
+        this = current_user(db, True)
+        if len(fields)==1 and fields[0]=='*':
+            fields = dbutils.dbfields['users']
+            if not this.userlevel.admin():
+                fields = list(set(fields).difference_update(_available_fields['users_get']['restricted']))
+        check_fields_access(fields, 'users_get', db)
+        users_ = db.execute("SELECT {} FROM users".format(', '.join(fields)), fields)
+        list(map(strdatetime, users_))
+        for user in users_:
+            if 'city_id' in user:
+                city = cities.get(user['city_id'], dbconnection=db)
+                user['city'] = {
+                    "city_id": city.city_id(),
+                    "title": city.title(),
+                    "geopoint": city.geopoint()}
+            if 'ampluas' in user:
+                amplua = ampluas.get(decode_set(user['ampluas']), dbconnection=db)
+                user['ampluas'] = [{"amplua_id":a.amplua_id(), "sport_type":a.sport_type(), "title":a.title()} for a in amplua]
+            if 'settings' in user:
+                user['settings'] = json.loads(user['settings'])
+        return dump({'count':len(users_), 'users':users_})
 
 
 @pages.get('/api/courts/get')
@@ -116,6 +192,12 @@ def courts_get():
     fields = bottle.request.query.get('fields') if 'fields' in bottle.request.query else 'court_id'
     fields = fields.split(',')
     with dbutils.dbopen() as db:
+        this = current_user(db, True)
+        if len(fields)==1 and fields[0]=='*':
+            fields = dbutils.dbfields['courts']
+            if not this.userlevel.admin():
+                fields = list(set(fields).difference_update(_available_fields['courts_get']['restricted']))
+        check_fields_access(fields, 'courts_get', db)
         courts = db.execute("SELECT {} FROM courts".format(', '.join(fields)), fields)
         list(map(strdatetime, courts))
         return dump({'count':len(courts), 'courts':courts})
@@ -124,12 +206,25 @@ def courts_get():
 @pages.get('/api/games/get')
 @handle_error
 @check_auth
-def courts_get():
+def games_get():
     fields = bottle.request.query.get('fields') if 'fields' in bottle.request.query else 'game_id'
     fields = fields.split(',')
     with dbutils.dbopen() as db:
+        this = current_user(db, True)
+        if len(fields)==1 and fields[0]=='*':
+            fields = dbutils.dbfields['courts']
+            if not this.userlevel.admin():
+                fields = list(set(fields).difference_update(_available_fields['courts_get']['restricted']))
+        check_fields_access(fields, 'courts_get', db)
         games = db.execute("SELECT {} FROM games".format(', '.join(fields)), fields)
         list(map(strdatetime, games))
+        for game in games:
+            if 'city_id' in game:
+                city = cities.get(game['city_id'], dbconnection=db)
+                game['city'] = {
+                    "city_id": city.city_id(),
+                    "title": city.title(),
+                    "geopoint": city.geopoint()}
         return dump({'count':len(games), 'games':games})
 
 
